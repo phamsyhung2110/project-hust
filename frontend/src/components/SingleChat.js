be78c37c42c00d9c0a6cc701926d7826823d4dc2
sotatek-hung.pham5 <hung.pham5@sotatek.com>
2023-06-07 18:26:37 +0700

    edit server cors socket

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -106,7 +106,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
   };
 
   useEffect(() => {
-    socket = io(ENDPOINT);
+    socket = io(ENDPOINT, {'transports': ['polling','websocket' ]});
     socket.emit("setup", user);
     socket.on("connected", () => setSocketConnected(true));
     socket.on("typing", () => setIsTyping(true));
